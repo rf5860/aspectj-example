fe7ed33b63f74208fdaeb8a261464ce7c9ad2f71
Robert Foy <robert.foy@gmail.com>
2019-01-10 13:12:31 +1100

    Made pointcut for constructor call explicit. Added pointcut to match specific packages

--- src/main/java/com/rjf/XStreamCreationAspect.java
+++ src/main/java/com/rjf/XStreamCreationAspect.java
@@ -5,6 +5,7 @@ import com.thoughtworks.xstream.XStream;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * An {@link Aspect} that hooks the {@link XStream} constructor, to initialise security.
@@ -14,7 +15,35 @@ import org.aspectj.lang.annotation.Aspect;
  */
 @Aspect
 public class XStreamCreationAspect {
-    @AfterReturning(value = "execution(public com.thoughtworks.xstream.XStream.new(..))")
+    /**
+     * The use of `call` may be marked as an error in IntelliJ.
+     * <br>
+     * This is because IntelliJ doesn't fully support "AspectJ".
+     * <br>
+     * Pointcut for all Constructor calls to {@link XStream}, regardless of arguments.
+     */
+    @Pointcut(value = "call(public com.thoughtworks.xstream.XStream.new(..))")
+    public void onXStreamCreation() { }
+
+    /**
+     * Pointcut for code called from classes in the package <pre>com.rjf</pre>
+     */
+    @Pointcut(value = "within(com.rjf..*)")
+    public void fromWithinSpecificPackage() { }
+
+
+    /**
+     * Advice to be called after method execution.
+     * <br>
+     * Matches the following pointcuts:
+     * <ul>
+     *     <li>Constructor calls of {@link XStream}</li>
+     *     <li>Code called within the package (and sub-packages) of <pre>com.rjf</pre></li>
+     * </ul>
+     *
+     * @param joinPoint runtime details of the current joinpoint
+     */
+    @AfterReturning(value = "onXStreamCreation() && fromWithinSpecificPackage()")
     public void after(final JoinPoint joinPoint) {
         System.out.printf("Executed:   %s%n", joinPoint.getSignature());
         System.out.printf("Returned:   %s%n", joinPoint.getThis());
---
+++ src/test/java/com/rjf/XStreamArgumentsProvider_ValidPackage.java
@@ -0,0 +1,38 @@
+package com.rjf;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.reflection.PureJavaReflectionProvider;
+import com.thoughtworks.xstream.core.ClassLoaderReference;
+import com.thoughtworks.xstream.core.DefaultConverterLookup;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+import com.thoughtworks.xstream.mapper.DefaultMapper;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+public class XStreamArgumentsProvider implements ArgumentsProvider {
+    private static final PureJavaReflectionProvider reflectionProvider = new PureJavaReflectionProvider();
+    private static final ClassLoader classLoader = XStream.class.getClassLoader();
+    private static final ClassLoaderReference classLoaderRef = new ClassLoaderReference(classLoader);
+    private static final DefaultMapper mapper = new DefaultMapper(classLoaderRef);
+    private static final StaxDriver driver = new StaxDriver();
+    private static final DefaultConverterLookup converter = new DefaultConverterLookup();
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(final ExtensionContext context) {
+        return Stream.of(new XStream(driver),
+                         new XStream(reflectionProvider),
+                         new XStream(reflectionProvider, driver),
+                         new XStream(reflectionProvider, mapper, driver),
+                         new XStream(reflectionProvider, driver, classLoader),
+                         new XStream(reflectionProvider, driver, classLoader, mapper),
+                         new XStream(reflectionProvider, driver, classLoader, mapper, converter, converter),
+                         new XStream(reflectionProvider, driver, classLoaderRef),
+                         new XStream(reflectionProvider, driver, classLoaderRef, mapper),
+                         new XStream(reflectionProvider, driver, classLoaderRef, mapper, converter, converter),
+                         new XStream()).map(Arguments::of);
+    }
+}
